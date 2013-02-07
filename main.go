c6775359cad445afc42ae2187246a4643081b7a9
michaljemala <michal.jemala@gmail.com>
2013-02-07 18:01:54 +0000

    DELETE&POST method refactored

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"code.google.com/p/gorilla/mux"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"sort"
 	"strconv"
+	"sync"
 )
 
 type (
@@ -14,19 +17,24 @@ type (
 		Title string `json:"title"`
 		Date  string `json:"date"`
 	}
+	Storage struct {
+		sync.RWMutex
+		data map[int]Session
+	}
 )
 
 var (
-	storage = make(map[int]Session)
+	storage  = Storage{data: make(map[int]Session)}
+	sequence = sequenceGenerator(1)
 )
 
 func init() {
-	// Sample data
-	storage[1] = Session{1, "Sess1", "06/02/2013"}
-	storage[2] = Session{2, "Sess2", "07/02/2013"}
-	storage[3] = Session{3, "Sess3", "08/02/2013"}
-	storage[4] = Session{4, "Sess4", "11/02/2013"}
-	storage[5] = Session{5, "Sess5", "12/02/2013"}
+	// Pre-generate sample data set
+	var idx int
+	for i := 1; i <= 5; i++ {
+		idx = <-sequence
+		storage.data[idx] = Session{idx, fmt.Sprintf("Sess%d", idx), "06/02/2013"}
+	}
 }
 
 func main() {
@@ -43,23 +51,49 @@ func main() {
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("www")))
 
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", logger(http.DefaultServeMux)))
 }
 
 func ListHandler(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Set("content-type", "application/json")
-
-	s := []Session{}
-	for _, v := range storage {
-		s = append(s, v)
+	storage.RLock()
+	var keys []int
+	for k := range storage.data {
+		keys = append(keys, k)
 	}
+	sort.Ints(keys)
+	sessions := []Session{}
+	for _, k := range keys {
+		sessions = append(sessions, storage.data[k])
+	}
+	storage.RUnlock()
 
+	rw.Header().Set("content-type", "application/json")
 	enc := json.NewEncoder(rw)
-	enc.Encode(&s)
+	enc.Encode(&sessions)
 }
 
+// Handlers
 func CreateHandler(rw http.ResponseWriter, r *http.Request) {
-	rw.WriteHeader(http.StatusInternalServerError)
+	var s Session
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&s); err != nil {
+		data, _ := json.Marshal(err)
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write(data)
+		return
+	}
+
+	s.Id = <-sequence
+
+	storage.Lock()
+	storage.data[s.Id] = s
+	storage.Unlock()
+
+	rw.Header().Set("content-type", "application/json")
+	rw.WriteHeader(http.StatusCreated)
+	enc := json.NewEncoder(rw)
+	enc.Encode(&s)
 }
 
 func GetHandler(rw http.ResponseWriter, r *http.Request) {
@@ -78,7 +112,27 @@ func DeleteHandler(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	delete(storage, int(id))
+	storage.Lock()
+	delete(storage.data, int(id))
+	storage.Unlock()
 
-	rw.WriteHeader(http.StatusOK)
+	rw.WriteHeader(http.StatusNoContent)
+}
+
+// Utils
+func sequenceGenerator(start int) <-chan int {
+	c := make(chan int)
+	go func(s int) {
+		for i := s; ; i++ {
+			c <- i
+		}
+	}(start)
+	return c
+}
+
+func logger(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(wr http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		h.ServeHTTP(wr, r)
+	})
 }
